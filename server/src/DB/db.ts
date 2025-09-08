79cd522564606bd6f538db4b009c0236964d77e8
ubu11 <uvaisubu11@gmail.com>
2025-09-08 13:21:26 +0530

    error reolved aggregateerror pg pool

--- server/src/DB/db.ts
+++ server/src/DB/db.ts
@@ -3,25 +3,30 @@ import { Pool } from "pg";
 dotenv.config({path:"../../.env"})
 
 
-  const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl:true
-})
+async function PgConnect() {
+    const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl:true
+  })
 
-   try {
-  await pool.connect()
-  console.log("connected")
+     try {
+    await pool.connect()
+    console.log("connected")
 
-} catch (error) {
-  if (error instanceof AggregateError){
-    console.log("multiple error occured")
-    for(const errors in error.errors){
-      console.log(errors)
-      process.exit(0);
+  } catch (error) {
+    if (error instanceof AggregateError){
+      console.log("multiple error occured")
+      for(const errors in error.errors){
+        console.log(errors)
+        process.exit(0);
+      }
     }
+    console.log(error)
+    process.exit(0);
   }
-  console.log(error)
-  process.exit(0);
+
+
+
 }
 
-
+export default PgConnect;
