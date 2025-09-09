ecafbd01f7dacdbc1fbaa97b7e524ec7e72acb34
ubu11 <uvaisubu11@gmail.com>
2025-09-09 19:51:56 +0530

    port changed

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,39 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import "dotenv/config";
+import { Hono } from "hono";
+import { Pool } from "pg";
 
-const app = new Hono()
+const app = new Hono().basePath("/api");
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL!,
+  ssl: true,
+});
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/home", async (c) => {
+  const client = await pool.connect();
+  try {
+    const { rows } = await client.query("SELECT version()");
+    return c.json({ version: rows[0].version });
+  } catch (error) {
+    console.error("Database query failed:", error);
+    return c.text("Failed to connect to database", 500);
+  } finally {
+    client.release();
+  }
+});
 
-serve({
+app.notFound((c) => {
+  return c.text("Custom 404 Message", 404);
+});
+
+const server = serve({
   fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
+  port: 5000,
+});
+
+
+process.on("SIGINT",()=>{
+  server.close(pool.end)
+  process.exit(0)
 })
+process.on()
