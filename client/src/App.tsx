c4b6bcc41028af16e3ec19cab4e20907f27ed175
ubu11 <uvaisubu11@gmail.com>
2025-09-11 21:23:29 +0530

    sidebar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,11 @@
 
-import { Button } from "./components/ui/button"
+
 
 function App() {
 
 
   return(<>
-  <h1>Style frameworks setup for build UI</h1>
-  <Button>click here</Button>
+
   </>)
 
 
