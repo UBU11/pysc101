315d414172ca4a75727728441155ba61a3708e77
ubu11 <uvaisubu11@gmail.com>
2025-10-09 13:57:12 +0530

    now card can add via through neondb console

--- client/src/components/main/Galery/Galery.tsx
+++ client/src/components/main/Galery/Galery.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 export default function Galery() {
   const [image, setImage] = useState("");
   const [title, setTitle] = useState("");
-  const [caption, setCaption] = useState("");
+  const [blog, setBlog] = useState([]);
 
   useEffect(() => {
     const getUser = async () =>
@@ -14,7 +14,7 @@ export default function Galery() {
         url: "http://localhost:3000/api/get",
       })
         .then((res) => {
-          console.log(res.data);
+          setBlog(blog.concat(res.data));
         })
         .catch((err) => {
           console.log(err);
@@ -25,170 +25,41 @@ export default function Galery() {
 
     getUser();
   }, []);
-
+  console.log(blog);
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 ml-32">
-      <TiltedCard
-        imageSrc={
-          image ||
-          "https://media.istockphoto.com/id/1055079680/vector/black-linear-photo-camera-like-no-image-available.jpg?s=612x612&w=0&k=20&c=P1DebpeMIAtXj_ZbVsKVvg-duuL0v9DlrOZUvPG6UJk="
-        }
-        altText="Kendrick Lamar - GNX Album Cover"
-        captionText="Kendrick Lamar - GNX"
-        containerHeight="300px"
-        containerWidth="300px"
-        imageHeight="300px"
-        imageWidth="300px"
-        rotateAmplitude={12}
-        scaleOnHover={1.2}
-        showMobileWarning={false}
-        showTooltip={true}
-        displayOverlayContent={true}
-        overlayContent={
-          <p
-            className="
-h-full w-full bg-violet-400 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-50 border p-1
+      {blog.map((card: any) => {
+        console.log(card);
+        return (
+          <TiltedCard
+            key={card.post_id}
+            imageSrc={
+              card.image_url ||
+              "https://media.istockphoto.com/id/1055079680/vector/black-linear-photo-camera-like-no-image-available.jpg?s=612x612&w=0&k=20&c=P1DebpeMIAtXj_ZbVsKVvg-duuL0v9DlrOZUvPG6UJk="
+            }
+            altText="Kendrick Lamar - GNX Album Cover"
+            captionText="Kendrick Lamar - GNX"
+            containerHeight="300px"
+            containerWidth="300px"
+            imageHeight="300px"
+            imageWidth="300px"
+            rotateAmplitude={12}
+            scaleOnHover={1.2}
+            showMobileWarning={false}
+            showTooltip={true}
+            displayOverlayContent={true}
+            overlayContent={
+              <p
+                className="
+h-full w-fit bg-violet-400 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-50 border p-1
 "
-          >
-            {title}
-          </p>
-        }
-      />
-
-      <TiltedCard
-        imageSrc={
-          image ||
-          "https://media.istockphoto.com/id/1055079680/vector/black-linear-photo-camera-like-no-image-available.jpg?s=612x612&w=0&k=20&c=P1DebpeMIAtXj_ZbVsKVvg-duuL0v9DlrOZUvPG6UJk="
-        }
-        altText="Kendrick Lamar - GNX Album Cover"
-        captionText="Kendrick Lamar - GNX"
-        containerHeight="300px"
-        containerWidth="300px"
-        imageHeight="300px"
-        imageWidth="300px"
-        rotateAmplitude={12}
-        scaleOnHover={1.2}
-        showMobileWarning={false}
-        showTooltip={true}
-        displayOverlayContent={true}
-        overlayContent={
-          <p
-            className="
-h-full w-full bg-violet-400 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-50 border p-1
-"
-          >
-            {title}
-          </p>
-        }
-      />
-
-      <TiltedCard
-        imageSrc={
-          image ||
-          "https://media.istockphoto.com/id/1055079680/vector/black-linear-photo-camera-like-no-image-available.jpg?s=612x612&w=0&k=20&c=P1DebpeMIAtXj_ZbVsKVvg-duuL0v9DlrOZUvPG6UJk="
-        }
-        altText="Kendrick Lamar - GNX Album Cover"
-        captionText="Kendrick Lamar - GNX"
-        containerHeight="300px"
-        containerWidth="300px"
-        imageHeight="300px"
-        imageWidth="300px"
-        rotateAmplitude={12}
-        scaleOnHover={1.2}
-        showMobileWarning={false}
-        showTooltip={true}
-        displayOverlayContent={true}
-        overlayContent={
-          <p
-            className="
-h-full w-full bg-violet-400 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-50 border p-1
-"
-          >
-            {title}
-          </p>
-        }
-      />
-
-      <TiltedCard
-        imageSrc={
-          image ||
-          "https://media.istockphoto.com/id/1055079680/vector/black-linear-photo-camera-like-no-image-available.jpg?s=612x612&w=0&k=20&c=P1DebpeMIAtXj_ZbVsKVvg-duuL0v9DlrOZUvPG6UJk="
-        }
-        altText="Kendrick Lamar - GNX Album Cover"
-        captionText="Kendrick Lamar - GNX"
-        containerHeight="300px"
-        containerWidth="300px"
-        imageHeight="300px"
-        imageWidth="300px"
-        rotateAmplitude={12}
-        scaleOnHover={1.2}
-        showMobileWarning={false}
-        showTooltip={true}
-        displayOverlayContent={true}
-        overlayContent={
-          <p
-            className="
-h-full w-full bg-violet-400 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-50 border p-1
-"
-          >
-            {title}
-          </p>
-        }
-      />
-
-      <TiltedCard
-        imageSrc={
-          image ||
-          "https://media.istockphoto.com/id/1055079680/vector/black-linear-photo-camera-like-no-image-available.jpg?s=612x612&w=0&k=20&c=P1DebpeMIAtXj_ZbVsKVvg-duuL0v9DlrOZUvPG6UJk="
-        }
-        altText="Kendrick Lamar - GNX Album Cover"
-        captionText="Kendrick Lamar - GNX"
-        containerHeight="300px"
-        containerWidth="300px"
-        imageHeight="300px"
-        imageWidth="300px"
-        rotateAmplitude={12}
-        scaleOnHover={1.2}
-        showMobileWarning={false}
-        showTooltip={true}
-        displayOverlayContent={true}
-        overlayContent={
-          <p
-            className="
-h-full w-full bg-violet-400 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-50 border p-1
-"
-          >
-            {title}
-          </p>
-        }
-      />
-
-      <TiltedCard
-        imageSrc={
-          image ||
-          "https://media.istockphoto.com/id/1055079680/vector/black-linear-photo-camera-like-no-image-available.jpg?s=612x612&w=0&k=20&c=P1DebpeMIAtXj_ZbVsKVvg-duuL0v9DlrOZUvPG6UJk="
-        }
-        altText="Kendrick Lamar - GNX Album Cover"
-        captionText="Kendrick Lamar - GNX"
-        containerHeight="300px"
-        containerWidth="300px"
-        imageHeight="300px"
-        imageWidth="300px"
-        rotateAmplitude={12}
-        scaleOnHover={1.2}
-        showMobileWarning={false}
-        showTooltip={true}
-        displayOverlayContent={true}
-        overlayContent={
-          <p
-            className="
-h-full w-full bg-violet-400 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-50 border p-1
-"
-          >
-            {title}
-          </p>
-        }
-      />
+              >
+                {card.title}
+              </p>
+            }
+          />
+        );
+      })}
     </div>
   );
 }
