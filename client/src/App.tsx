d613593c56eb48e44b41e7c7733a51606c0f42ee
ubu11 <uvaisubu11@gmail.com>
2025-10-11 20:08:25 +0530

    hero section card is added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import PixelBlast from "./components/main/background/pixelblast";
 import Galery from "./components/main/Galery/Galery";
 import { Navbar } from "./components/main/navbar/navbar";
+import CardSwap,{Card} from "./components/main/hero/CardSwap";
 function App() {
   return (
     <>
@@ -9,7 +10,29 @@ function App() {
         <Navbar />
         <div className="relative w-full h-screen">
           <PixelBlast />
-
+          <div className=" absolute inset-0">
+          <div style={{ height: '600px', position: 'relative' }}>
+  <CardSwap
+    cardDistance={60}
+    verticalDistance={70}
+    delay={5000}
+    pauseOnHover={false}
+  >
+    <Card>
+      <h3>Card 1</h3>
+      <p>Your content here</p>
+    </Card>
+    <Card>
+      <h3>Card 2</h3>
+      <p>Your content here</p>
+    </Card>
+    <Card>
+      <h3>Card 3</h3>
+      <p>Your content here</p>
+    </Card>
+  </CardSwap>
+</div>
+          </div>
           <div className="absolute inset-x-0 bottom-0 h-16">
             <Galery />
           </div>
