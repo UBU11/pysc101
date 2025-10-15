351b02a93d3da8f7dfd1a6090a484cf817e2db85
ubu11 <uvaisubu11@gmail.com>
2025-10-15 21:49:04 +0530

    add

--- client/src/components/main/hero/main.tsx
+++ client/src/components/main/hero/main.tsx
@@ -3,7 +3,7 @@ import CardSwap, { Card } from "./CardSwap";
 import axios from "axios";
 import { useState } from "react";
 
-const [img, setImage] = useState<String>("");
+const [image, setImage] = useState<String[]>([]);
 useEffect(() => {
   const getData = async () =>
     axios({
@@ -12,12 +12,12 @@ useEffect(() => {
     })
       .then((res) => {
         console.log(res.data);
-        return setImage(res.data[0].image_url);
+        return setImage(image.concat(res.data));
       })
       .catch((err) => console.log("error is occured:", err))
       .finally(() => console.log("successfull"));
 
-      getData()
+  getData();
 }, []);
 
 export default function Main() {
@@ -28,18 +28,12 @@ export default function Main() {
       delay={5000}
       pauseOnHover={false}
     >
-      <Card>
-        <img src="" alt="" />
-        <p>Your content here</p>
-      </Card>
-      <Card>
-        <h3>Card 2</h3>
-        <p>Your content here</p>
-      </Card>
-      <Card>
-        <h3>Card 3</h3>
-        <p>Your content here</p>
-      </Card>
+      {image.map((image) => (
+        <Card>
+          <img src={image.image_url} alt="image is loading....." />
+          <p>Your content here</p>
+        </Card>
+      ))}
     </CardSwap>
   );
 }
