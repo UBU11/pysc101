91d29da7818e15db1d279b29c45eb1d5c3e8f2db
ubu11 <uvaisubu11@gmail.com>
2025-11-01 11:16:53 +0530

    site working!

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	// site: "https://fuwari.vercel.app/",
+	site: "http://localhost:4321",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
